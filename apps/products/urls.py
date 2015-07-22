1c844b8fd3651261378cb2be771a6bf98cc4f3d1
Matthew Han <matthew_han@outlook.com>
2015-07-22 16:55:34 -0700

    removed old code

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,8 +4,6 @@ from apps.products.views import ProductsView, ProductView
 from apps.products import views
 urlpatterns = [
     url(r'^$', ProductsView.as_view()),
-    #url(r'^products/', ProductsView.as_view()),
     url(r'^(?P<product_id>\d+)/$', ProductView.as_view()),
     url(r'^(?P<product_id>\d+)/edit', views.edit, name = 'edit'),
-    # url(r'^products/(?P<product_id>\d+)/$', Product.as_view(), name = 'product'),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -16,7 +16,6 @@ class ProductsView(View):
     def post(self, request):
         form = ProductForm(request.POST)
         if form.is_valid():
-            print 'YAY VALID CREATE'
             name = request.POST.get("name")
             manufacturer = request.POST.get("manufacturer")
             price = request.POST.get("price")
@@ -24,7 +23,6 @@ class ProductsView(View):
             product = Product.objects.create(name = name, manufacturer = manufacturer, price = price, description = description)
             return redirect('/products')
         else:
-            print 'OH HELL NO'
             products = Product.objects.all()
             context = {
                 'products': products,
@@ -44,10 +42,10 @@ class ProductView(View):
     def post(self, request, product_id):
         #Update Method
         if request.POST.get("_method") == 'put':
-            print 'in ProductView Post Update'
+            #print 'in ProductView Post Update'
             form = ProductForm(request.POST)
             if form.is_valid():
-                print 'in ProductView Post Update Form Valid'
+                #print 'in ProductView Post Update Form Valid'
                 name = request.POST.get("name")
                 manufacturer = request.POST.get("manufacturer")
                 price = request.POST.get("price")
@@ -55,31 +53,17 @@ class ProductView(View):
                 Product.objects.filter(id=product_id).update(name = name, manufacturer = manufacturer, price = price, description = description)
                 return redirect('/products')
             else:
-                print 'in ProductView Post Update Form Invalid'
+                #print 'in ProductView Post Update Form Invalid'
                 context = {
                     'form': form,
                 }
                 return render(request, 'products/edit.html', context)
         #Destroy Method
         else:
-            print 'in ProductView Post Delete'
+            #print 'in ProductView Post Delete'
             Product.objects.filter(id=product_id).delete()
             return redirect('/products')
 
-# def index(request):
-#     products = Product.objects.all()
-#     data = {
-#         'products': products,
-#         #In Restful this would be in def new
-#         'form':AddProductForm(),
-#     }
-#     return render(request, 'products/index.html', data)
-# def product(request, product_id):
-#     # user = User.objects.get(id=user_id)
-#     # context = {
-#     #     "user" : user,
-#     # }
-#     return render(request, 'products/show.html')
 def edit(request, product_id):
     product = Product.objects.get(id=product_id)
     form = ProductForm(initial={'name':product.name,'manufacturer':product.manufacturer,'price':product.price,'description':product.description})
@@ -88,39 +72,3 @@ def edit(request, product_id):
         "product":product,
     }
     return render(request, 'products/edit.html', context)
-# def update(request, product_id):
-#     product = Product.objects.get(id=product_id)
-#     context = {
-#         "product" : product,
-#     }
-#     return render(request, 'products/edit.html')
-# def create(request):
-#     if request.method == "POST":
-#         form = AddProductForm(request.POST)
-#         if form.is_valid():
-#             print 'YAY VALID CREATE'
-#             name = request.POST.get("name")
-#             manufacturer = request.POST.get("manufacturer")
-#             price = request.POST.get("price")
-#             description = request.POST.get("description", False)
-#             product = Product.objects.create(name = name, manufacturer = manufacturer, price = price, description = description)
-#             return redirect(index)
-#         else:
-#             print 'OH HELL NO'
-#             context = {
-#                 'form':form,
-#             }
-#             return render(request, 'products/index.html', context)
-#
-#     else:
-#         form = AddProductForm()
-#         context = {
-#             'form':form,
-#         }
-#         return render(request, 'products/index.html', context)
-# def destroy(request):
-#     return redirect(index)
-#     # user = User.objects.get(id=user_id)
-#     # context = {
-#     #     "user" : user,
-#     # }
